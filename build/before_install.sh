e25d2325423c64764d5d3257236488e2d972786d
hjkcai <huajingkun1214@gmail.com>
2017-09-10 00:58:14 +0800

    修复更新 hexo 分支时导致文章消失的问题

--- build/before_install.sh
+++ build/before_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
+git submodule update --remote --recursive
 openssl aes-256-cbc -K $encrypted_0e425c9ccbd3_key -iv $encrypted_0e425c9ccbd3_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc
 chmod 600 deploy-key
